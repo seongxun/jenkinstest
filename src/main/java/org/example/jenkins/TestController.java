758bcac2c17993bb030bc4da29082d1bbedd08b5
seongxun <rjqlrl30@naver.com>
2024-08-23 16:38:41 +0900

    test

--- src/main/java/org/example/jenkins/TestController.java
+++ src/main/java/org/example/jenkins/TestController.java
@@ -10,6 +10,6 @@ public class TestController {
     @GetMapping("/version")
     public ResponseEntity<String> test(){
         System.out.println("hi");
-        return ResponseEntity.ok("v3");
+        return ResponseEntity.ok("v4");
     }
 }
